041b2a32998f06f40c2ebc6e1362c815d7df1c3b
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-08-20 21:31:34 +0200

    task id 11, sales controller initial sub menu structure

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -14,3 +14,38 @@ def run():
     """
 
     # your code
+    DB_FILENAME = 'model/sales/sales.csv'
+    options = ["Add a new sale",
+               "Remove a sale",
+               "Update a sale",
+               "See Id of item which sold for the lowest price",
+               "See item sold in a specific date range"]
+    title_list = ['Id',
+                  'Title',
+                  'Price',
+                  'Month',
+                  'Day',
+                  'Year']
+    is_running = True
+    while is_running is True:
+        table = common.get_table(DB_FILENAME)
+        terminal_view.print_table(table, title_list)
+        choice = terminal_view.get_choice(
+            'Sales menu',
+            options,
+            'Back to main menu')
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "0":
+            is_running = False
+        else:
+            terminal_view.print_error_message("There is no such choice.")
