164c3121ae431128d2fd3bdc2662fced77ec13ad
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-08-20 17:22:20 +0200

    task id 23, common(controller) save table to file function

--- controller/common.py
+++ controller/common.py
@@ -8,5 +8,9 @@ def get_table(file_name):
     return data_manager.get_table_from_file(file_name)
 
 
+def save_table_to_file(table, file_name):
+    data_manager.write_table_to_file(file_name, table)
+
+
 def find_id(table, index):
     return table[index][0]
